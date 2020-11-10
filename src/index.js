1710f4f6c00e3c9956756bf5625556e3179bfa74
Petar Georgiev <kapito929@gmail.com>
2020-11-10 16:38:09 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { createLogger } from 'redux-logger';
 import 'tachyons';
 
 import App from './containers/App';
-import serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
 import { requestRobots, searchRobots } from './reducers'
 
 import './index.css';
@@ -24,6 +24,4 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-serviceWorker.register();
-
+registerServiceWorker();
